142773f7caae86889b4b8fef036556b399fa96bb
Siti Nahlah Salsabila <151691753+bedaddict@users.noreply.github.com>
2026-09-06 17:26:44 +0700

    migration to pgsql

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -16,6 +16,7 @@ class AdminSeeder extends Seeder
             [
                 'name' => 'Admin Toko',
                 'password' => Hash::make('password123'),
+                'role' => 'Admin',
             ]
         );
     }
